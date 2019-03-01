f0f868e7e32c61e6a488ecb7c86252442d2dee1d
Matthew Chalcraft <matt.jumpinjive@gmail.com>
2019-02-28 20:57:06 -0600

    adding player

--- src/resources/Player.java
+++ src/resources/Player.java
@@ -6,14 +6,20 @@ import java.util.ArrayList;
 
 public class Player {
 
-    ArrayList<Card> hand;
+    ArrayList<Card> hand; // The players current hand
     Boolean action_state;
-    Boolean game_state;
-    int card_sum;
-    int money;
+    Boolean game_state; // True = int, False = out of the game
+    Boolean round_state; // True = int, False = out of the round
+    int card_sum; // Sum of the current cards in the players hand
+    int money; // Current money that the player has
+    int bet; // Players current bet
 
     Player(){
-
+        card_sum = 0;
+        money = 1000;
+        bet = 0;
+        game_state = true;
+        round_state = true;
     }
 
 }
